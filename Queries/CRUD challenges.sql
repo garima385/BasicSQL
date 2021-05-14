ce0c080cc4a4fa63d7623232ea5ecf598d671a7e
garima385 <84167328+garima385@users.noreply.github.com>
2021-05-14 06:24:56 +0100

    Update CRUD challenges.sql

--- Queries/CRUD challenges.sql
+++ Queries/CRUD challenges.sql
@@ -13,7 +13,7 @@ shirt_id	article			color	shirt_size	last_worn
 8			tank top		blue	M			15
 
 /*create table shirts*/
-CREATE TABLE shirts(
+CREATE TABLE gshirts(
 	shirt_id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
 	article VARCHAR(50),
 	color VARCHAR(50),
@@ -23,10 +23,10 @@ CREATE TABLE shirts(
 
 SHOW TABLES;
 
-DESC shirts;
+DESC gshirts;
 
 /*add values*/
-INSERT INTO shirts(article,color,shirt_size,last_worn)
+INSERT INTO gshirts(article,color,shirt_size,last_worn)
 VALUES ('t-shirt', 'white', 'S', 10),
 ('t-shirt', 'green', 'S', 200),
 ('polo shirt', 'black', 'M', 10),
@@ -38,49 +38,49 @@ VALUES ('t-shirt', 'white', 'S', 10),
 
 
 /*Get All That Data In There With a single line*/
-SELECT * FROM shirts;
+SELECT * FROM gshirts;
 
 
 /*Add A New Shirt
 Purple polo shirt, size M last worn 50 days ago*/
-INSERT INTO shirts(article,color,shirt_size,last_worn)
+INSERT INTO gshirts(article,color,shirt_size,last_worn)
 VALUES('polo shirt','purple','M',50);
 
 
 /*SELECT all shirts
 But Only Print Out Article and Color*/
 SELECT article,color
-FROM shirts;
+FROM gshirts;
 
 
 /*SELECT all medium shirts
 Print Out Everything But shirt_id*/
-SELECT article,color,shirt_size,last_worn FROM shirts
+SELECT article,color,shirt_size,last_worn FROM gshirts
 WHERE shirt_size = 'M';
 
 
 /*Update all polo shirts
 Change their size to L*/
-UPDATE shirts
+UPDATE gshirts
 SET shirt_size = 'L'
 WHERE article = 'polo shirt';
 
 
 /*Update the shirt last worn 15 days ago
 Change last_worn to 0*/
-UPDATE shirts
+UPDATE gshirts
 SET last_worn = 0
 WHERE last_worn = 15;
 
 
 /*alter table of shirtsize*/
-ALTER TABLE shirts
+ALTER TABLE gshirts
 MODIFY COLUMN shirt_size VARCHAR(2);
 
 
 /*Update all white shirts
 Change size to 'XS' and color to 'off white'*/
-UPDATE shirts
+UPDATE gshirts
 SET color = 'off white',
 	shirt_size = 'XS'
 WHERE color = 'white';
@@ -88,15 +88,15 @@ WHERE color = 'white';
 
 /*Delete all old shirts
 Last worn 200 days ago*/
-DELETE FROM shirts
+DELETE FROM gshirts
 WHERE last_worn = 200;
 
 
 /*Delete all tank tops
 Your tastes have changed...*/
-DELETE FROM shirts
+DELETE FROM gshirts
 WHERE article = 'tank top';
 
 
 /*Delete all shirts*/
-DELETE FROM shirts;
+DELETE FROM gshirts;
